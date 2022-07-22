4399fce1fbdc22de19f10d854b34b1c09f949130
Bruno <bruno_zangi@hotmail.com>
2022-07-21 23:34:39 -0300

    Revert "Add GraphQL API"
    This reverts commit 4e6b4b4cdc6af297d60128038c19bfe70ffb3d7d.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,3 @@
 //= link_directory ../stylesheets .css
 //= link_directory ../ .js
 //= link application.js
-//= link graphiql/rails/application.css
-//= link graphiql/rails/application.js
--- app/graphql/types/mutation_type.rb
+++
@@ -1,7 +0,0 @@
-module Types
-  class MutationType < Types::BaseObject
-    field :create_user, mutation: Mutations::User::CreateUserMutation
-    field :update_user, mutation: Mutations::User::UpdateUserMutation
-    field :destroy_user, mutation: Mutations::User::DestroyUserMutation
-  end
-end
